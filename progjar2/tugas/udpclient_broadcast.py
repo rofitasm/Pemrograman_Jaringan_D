72cd2ed5bf9e04f750e3ee590ea7f83269cfea46
Rofita Siti Musdalifah <rofitasitimusdalifah@gmail.com>
2021-05-22 00:00:55 +0700

    udp broadcast

---
+++ progjar2/tugas/udpclient_broadcast.py
@@ -0,0 +1,22 @@
+import socket
+import time
+
+
+#TARGET_IP = '255.255.255.255'
+TARGET_IP = '192.168.122.0' #batasi scope pengiriman broadcast
+TARGET_PORT = 5005
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEPORT, 1)
+sock.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST, 1)
+
+
+angka = 0
+while True:
+    angka = angka+1
+    msg = " BROADCAST ini angka {} " . format(angka)
+    print(msg)
+    sock.sendto(msg.encode(), ("192.168.122.0", TARGET_PORT))
+#    sock.sendto(msg.encode(), ("255.255.255.255", TARGET_PORT))
+#    time.sleep(1)
